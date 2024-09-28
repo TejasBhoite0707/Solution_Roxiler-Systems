820b7bad0054ba7e1e3ad916fba593fbdbafef6e
Pranav Pise <160746099+TheCaptain1810@users.noreply.github.com>
2024-09-28 15:34:57 +0530

    mongodb database connected and data saved

--- Backend_Task/index.js
+++ Backend_Task/index.js
@@ -1,11 +1,28 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const fetchAndSaveData = require("./controllers/getData");
 const app = express();
 const port = 3000;
+dotenv.config();
 
 // Middleware to parse JSON bodies
 app.use(express.json());
+app.use(cors());
+
+let mongourl = process.env.Mongo_URI;
+
+mongoose
+  .connect(mongourl)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    // Fetch and save data after successful connection
+    fetchAndSaveData();
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
 
 // Basic route
 app.get("/", (req, res) => {
